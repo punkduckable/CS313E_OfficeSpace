1d52a90a7d70a587d46f8d45d407965b041fea3c
Robert Stephany <robert.stephany@utexas.edu>
2019-06-17 19:23:15 -0500

    Added an Interval Union class
    This class is used to store unions of Intervals.
    
    I plan on using this to implement the "sweep" method for finding the area of the rectangles.

--- OfficeSpace.py
+++ OfficeSpace.py
@@ -1,5 +1,7 @@
 # Closed interval class
 class Interval(object):
+    """ This class models closed intervals """
+
     ############################################################################
     # Constructor
     def __init__(self, a, b):
@@ -14,13 +16,29 @@ class Interval(object):
 
     ############################################################################
     # Other methods
+
+    def intersects(self, other):
+        """ This method returns wheather or not self and other intersect.
+
+        let [a1, b1] , [a2, b2] be two intervals. These intervals intersect as
+        long as max(a1, a2) <= min(b1, b2) """
+        a = max(self.a, other.a);
+        b = min(self.b, other.b);
+
+        if(a <= b):
+            return True;
+        else:
+            return False;
+
+
     def intersection(self, other):
         """This method finds the intersection of two intervals. If the
         intersection is non-empty then it is returned (as an interval).
         Otherwise, if the intersection is empty, then none is returned.
 
-        let [a1, b1] , [a2, b2] be two intervals. They intersect as long as
-        max(a1, a2) <= min(b1, b2). """
+        let [a1, b1] , [a2, b2] be two intervals. The two intervals intersect
+        if max(a1, a2) <= min(b1, b2). In this case, their intersection is
+        [max(a1,a2), min(b1, b2)] """
         a = max(self.a, other.a);
         b = min(self.b, other.b);
 
@@ -28,6 +46,9 @@ class Interval(object):
             return Interval(a,b);
         else:
             return None;
+
+    def __len__(self):
+        return self.b - self.a;
 
     ############################################################################
     # Special methods
@@ -40,3 +61,67 @@ class Interval(object):
 
     def __ne__(self, other):
         return not (self == other);
+
+
+
+# Interval union class
+# This class models the union of closed intervals. In general, the union of two
+# closed intervals is not a closed interval. Thus, we have a separate class
+# just to deal with this situation.
+class Interval_Union(object):
+    """an Interval_Union object contains a single data member,
+    its Interval_List. This contains a set of non-intersecting closed intervals.
+    since the intervals are non-overlapping, we can easily find the total
+    length of the Interval_Union by summing up the lengths of the intervals
+    in the Interval_List """
+
+    ############################################################################
+    # Constructor:
+    def __init__(self):
+        # Start off with an empty Interval_List.
+        self.Interval_List = [];
+
+    ############################################################################
+    # append (and helper function(s))
+    def _intersects_with_interval_in_list(self, I):
+        # This method determines if the interval I intersects with any of the
+        # intervals in intervals in the Interval_List. If so, then this function
+        # returns the index of the first item in the Interval_List that
+        # intersects with I. Otherwise, a -1 is returned.
+        for i in range(len(self.Interval_List)):
+            if(self.Interval_List[i].intersects(I)):
+                return i;
+
+        # If we get to here, then I does not intersect with any of the intervals
+        # in the list, return -1.
+        return -1;
+
+    def append(self, I):
+        # This method appends another interval, I, onto the Interval list.
+
+        # First, we need to figure out if I intersects with any of the intervals
+        # in the Interval list (see above).
+        i = self._intersects_with_interval_in_list(I)
+        if(i != -1):
+            # If so, then we need to find the union of I with the ith
+            # interval, remove the ith interval from the Interval List, and then
+            # append the combined result onto self.
+            ith_interval = self.Interval_List.pop(i);
+            a = min(ith_interval.a, I.a);
+            b = max(ith_interval.b, I.b);
+
+            combined_interval = Interval(a,b);
+            self.append(combined_interval);
+        else:
+            # if not, then I is disjoint from every interval in the Interval
+            # List, we can safely append I onto the Interval List.
+            self.Interval_List.append(I);
+
+    #############################################################################
+    # special member functions
+    def __len__(self):
+        sum = 0;
+        for i in range(len(self.Interval_List)):
+            sum += len(self.Interval_List[i]);
+
+        return sum;
--- Test.py
+++ Test.py
@@ -1,8 +1,8 @@
-from OfficeSpace import Interval;
+from OfficeSpace import Interval, Interval_Union;
 import unittest;
 
 # Interval tests.
-class IntervalTests(unittest.TestCase):
+class Interval_Tests(unittest.TestCase):
     def test_equal(self):
         I1 = Interval(0,1);
         I2 = Interval(0,1);
@@ -24,6 +24,40 @@ class IntervalTests(unittest.TestCase):
         I4 = Interval(2,3);
         # The intersection of [0,1] and [2,3] should be the empty set (None)
         self.assertIsNone(I1.intersection(I4));
+
+    def test_length(self):
+        I1 = Interval(2,3);
+        self.assertEqual(len(I1), 1);
+
+
+# Interval Union tests.
+class Interval_Union_Tests(unittest.TestCase):
+    def test_append(self):
+        U = Interval_Union();
+
+        I1 = Interval(0,2);
+        I2 = Interval(1,3);
+        I3 = Interval(3,5);
+        I4 = Interval(10,12);
+
+        U.append(I1);
+        U.append(I2);
+        U.append(I3);
+        U.append(I4);
+
+        # Based on these three intervals, their union should be two intervals,
+        # [0,5] and [10,12], with a total length of 7. First, lets check the
+        # lenght
+        self.assertEqual(len(U), 7);
+
+        # Next, let's make sure that U's Interval list contains 2 elements.
+        self.assertEqual(len(U.Interval_List),2);
+
+        # Finally, let's check that the two intervals in the Interval List are
+        # indeed [0,5] and [10,12].
+        self.assertIn(Interval(0,5), U.Interval_List);
+        self.assertIn(Interval(10,12), U.Interval_List);
+
 
 if(__name__ == "__main__"):
     unittest.main();
